0129d71f9d517f003891189c3a6ef1326bbdc7c9
watcharaphanPrima <watcharaphan.prima@gmail.com>
2025-10-28 12:42:22 +0700

    delete old tab

--- pages/MarketingPage.tsx
+++ pages/MarketingPage.tsx
@@ -57,7 +57,7 @@ const hasAdminAccess = (user: User) => {
 const MarketingPage: React.FC<MarketingPageProps> = ({ currentUser }) => {
   const [activeTab, setActiveTab] = useState<
     "ads" | "userManagement" | "adsInput" | "dashboard"
-  >("ads");
+  >("dashboard");
   const [pages, setPages] = useState<Page[]>([]);
   const [promotions, setPromotions] = useState<Promotion[]>([]);
   const [adSpend, setAdSpend] = useState<AdSpend[]>([]);
@@ -807,7 +807,7 @@ const MarketingPage: React.FC<MarketingPageProps> = ({ currentUser }) => {
             </button>
             <button
               onClick={() => setActiveTab("ads")}
-              className={`py-2 px-1 border-b-2 font-medium text-sm ${
+              className={`hidden py-2 px-1 border-b-2 font-medium text-sm ${
                 activeTab === "ads"
                   ? "border-emerald-500 text-emerald-600"
                   : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
